247adeb22224fce57ac3ee3c5cfa1fed15d6a314
Masahiro Nagano <kazeburo@gmail.com>
2013-10-24 13:47:18 +0900

    pod

--- README.md
+++ README.md
@@ -26,9 +26,68 @@ Plack::Middleware::Session::Simple - Make Session Simple
 # DESCRIPTION
 
 Plack::Middleware::Session::Simple is a yet another session management module.
-This module supports psgix.session and psgi.session.options. And compatible with
-Plack::Middleware::Session.
-This middleware can reduce unnecessary accessing to cache Store and Set-Cookie header
+This middleware supports psgix.session and psgi.session.options. 
+Plack::Middleware::Session::Simple has compatibility with Plack::Middleware::Session 
+and you can reduce unnecessary accessing to cache Store and Set-Cookie header.
+
+This module uses Cookie to keep session state. does not support URI based session state.
+
+# OPTIONS
+
+- cache
+
+    cache object instance that has get, set, and remove methods.
+
+- session\_key
+
+    This is the name of the session key, it defaults to 'simple\_session'.
+
+- keep\_empty
+
+    If disabled, Plack::Middleware::Session::Simple does not output Set-Cookie header and store session until session are used. You can reduce Set-Cookie header and access to session store that is not required. (default: true)
+
+        builder {
+            enable 'Session::Simple',
+                cache => Cache::Memcached::Fast->new({servers=>[..]}),
+                session_key => 'myapp_session',
+                keep_empty => 0;
+            mount '/' => sub {
+                my $env = shift;
+                [200,[], ["ok"]];
+            },
+            mount '/login' => sub {
+                my $env = shift;
+                $env->{'psgix.session'}->{user} = 'session user'
+                [200,[], ["login"]];
+            },
+        };
+        
+
+        my $res = $app->(req_to_psgi(GET "/")); #res does not have Set-Cookie
+        
+
+        my $res = $app->(req_to_psgi(GET "/login")); #res has Set-Cookie
+
+- path
+
+    Path of the cookie, this defaults to "/";
+
+- domain
+
+    Domain of the cookie, if nothing is supplied then it will not be included in the cookie.
+
+- expires
+
+    Cookie's expires date time. several formats are supported. see [Cookie::Baker](http://search.cpan.org/perldoc?Cookie::Baker) for details.
+    if nothing is supplied then it will not be included in the cookie, which means the session expires per browser session.
+
+- secure
+
+    Secure flag for the cookie, if nothing is supplied then it will not be included in the cookie.
+
+- httponly
+
+    HttpOnly flag for the cookie, if nothing is supplied then it will not be included in the cookie.
 
 # LICENSE
 
--- lib/Plack/Middleware/Session/Simple.pm
+++ lib/Plack/Middleware/Session/Simple.pm
@@ -100,9 +100,9 @@ sub finalize {
 
     if ( $need_store ) {
         if ($options->{expire}) {
-            $self->cache->delete($options->{id});
+            $self->cache->remove($options->{id});
         } elsif ($options->{change_id}) {
-            $self->cache->delete($options->{id});
+            $self->cache->remove($options->{id});
             $options->{id} = $self->generate_id($env);
             $self->cache->set($options->{id}, $session->untie);
         } else {
@@ -218,9 +218,70 @@ Plack::Middleware::Session::Simple - Make Session Simple
 =head1 DESCRIPTION
 
 Plack::Middleware::Session::Simple is a yet another session management module.
-This module supports psgix.session and psgi.session.options. And compatible with
-Plack::Middleware::Session.
-This middleware can reduce unnecessary accessing to cache Store and Set-Cookie header
+This middleware supports psgix.session and psgi.session.options. 
+Plack::Middleware::Session::Simple has compatibility with Plack::Middleware::Session 
+and you can reduce unnecessary accessing to cache Store and Set-Cookie header.
+
+This module uses Cookie to keep session state. does not support URI based session state.
+
+=head1 OPTIONS
+
+=over 4
+
+=item cache
+
+cache object instance that has get, set, and remove methods.
+
+=item session_key
+
+This is the name of the session key, it defaults to 'simple_session'.
+
+=item keep_empty
+
+If disabled, Plack::Middleware::Session::Simple does not output Set-Cookie header and store session until session are used. You can reduce Set-Cookie header and access to session store that is not required. (default: true)
+
+    builder {
+        enable 'Session::Simple',
+            cache => Cache::Memcached::Fast->new({servers=>[..]}),
+            session_key => 'myapp_session',
+            keep_empty => 0;
+        mount '/' => sub {
+            my $env = shift;
+            [200,[], ["ok"]];
+        },
+        mount '/login' => sub {
+            my $env = shift;
+            $env->{'psgix.session'}->{user} = 'session user'
+            [200,[], ["login"]];
+        },
+    };
+    
+    my $res = $app->(req_to_psgi(GET "/")); #res does not have Set-Cookie
+    
+    my $res = $app->(req_to_psgi(GET "/login")); #res has Set-Cookie
+
+=item path
+
+Path of the cookie, this defaults to "/";
+
+=item domain
+
+Domain of the cookie, if nothing is supplied then it will not be included in the cookie.
+
+=item expires
+
+Cookie's expires date time. several formats are supported. see L<Cookie::Baker> for details.
+if nothing is supplied then it will not be included in the cookie, which means the session expires per browser session.
+
+=item secure
+
+Secure flag for the cookie, if nothing is supplied then it will not be included in the cookie.
+
+=item httponly
+
+HttpOnly flag for the cookie, if nothing is supplied then it will not be included in the cookie.
+
+=back 
 
 =head1 LICENSE
 
